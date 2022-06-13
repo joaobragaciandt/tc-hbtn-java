38e99e0529c97891e7aa711226cd05d1a69bfda6
joaobragaciandt <joaobraga@ciandt.com>
2022-06-13 17:10:57 -0300

    Java - Tipos Primitivos e Wrappers

---
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -0,0 +1,48 @@
+public class InteiroPositivo {
+    private Integer valor;
+
+    public InteiroPositivo(Integer valor) throws Exception {
+        ehPositivo(valor);
+        this.valor = valor;
+    }
+
+    public InteiroPositivo(String valor) throws Exception  {
+        ehPositivo(Integer.parseInt(valor));
+        this.valor = Integer.parseInt(valor);
+    }
+
+    public Exception ehPositivo(Integer valor) throws Exception {
+        if(valor<0) {
+            throw new Exception("Valor nao eh um valor inteiro positivo");
+        }
+        return null;
+    }
+
+    public void setValor(Integer valor) throws Exception {
+        ehPositivo(valor);
+        this.valor = valor;
+    }
+
+    public Integer getValor() throws Exception{
+        return this.valor;
+    }
+
+    public boolean ehPrimo() {
+        if( valor == 1) {
+            return false;
+        }
+
+        if(valor != 2 && valor%2 == 0) {
+            return false;
+        }
+
+        for(int i=3;i<(valor/2); i+=2) {
+            if(valor%i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
