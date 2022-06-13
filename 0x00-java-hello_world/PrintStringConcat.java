82362a3123bea5cfb2782e7f105a3acdf7cfb513
joaobragaciandt <joaobraga@ciandt.com>
2022-06-13 11:45:35 -0300

    PrintStringConcat.java

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,7 @@
+public class PrintStringConcat {
+    public static void main(String args[]) {
+        String texto1 = "Holberton";
+        String texto2 = "School";
+        System.out.printf("%s\n", "Bem vindo a " + texto1+" "+texto2+ "!");
+    }
+}
