61f2cef01abb425bcf353f27f5d0ce813fa604f1
joaobragaciandt <joaobraga@ciandt.com>
2022-06-13 10:48:06 -0300

    Adjustments

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable {
     public static void main(String args[]) {
         double pi = 3.14159;
-        System.out.printf("%.3f\n", pi);
+        System.out.printf("Valor de pi: %.3f\n", pi);
     }
 }
