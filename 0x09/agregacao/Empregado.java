4f43a6df66ea2845eaf2f029e906fdf92348b5a9
joaobragaciandt <joaobraga@ciandt.com>
2022-06-14 15:14:57 -0300

    0. Empregado/Endereço

---
+++ 0x09/agregacao/Empregado.java
@@ -0,0 +1,43 @@
+public class Empregado {
+    private String nome;
+    private int codigo;
+    private Endereco endereco;
+
+    public Empregado(String nome, int codigo, Endereco endereco) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.endereco = endereco;
+    }
+
+    public void apresentar() {
+        System.out.printf("Codigo: %d\n",getCodigo());
+        System.out.printf("Nome: %s\n",getNome());
+        System.out.printf("Bairro: %s\n",getEndereco().getBairro());
+        System.out.printf("Cidade: %s\n",getEndereco().getCidade());
+        System.out.printf("Pais: %s\n",getEndereco().getPais());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
