ff4ce7d3f1d8755464b28ff7fd97b174309c413d
joaobragaciandt <joaobraga@ciandt.com>
2022-06-14 09:20:45 -0300

    2. Métodos

---
+++ 0x04/metodos/PersonagemGame.java
@@ -0,0 +1,38 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        if(quantidadeDeDano>=getSaudeAtual()) {
+            setSaudeAtual(0);
+        }
+        else {
+            setSaudeAtual(getSaudeAtual()-quantidadeDeDano);
+        }
+    }
+
+    public void receberCura(int quatidadeDeCura) {
+        if(quatidadeDeCura + getSaudeAtual()>= 100) {
+            setSaudeAtual(100);
+        }
+        else {
+            setSaudeAtual(getSaudeAtual()+quatidadeDeCura);
+        }
+    }
+}
