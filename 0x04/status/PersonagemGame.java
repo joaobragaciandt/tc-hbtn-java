20c91f9fdefa89db61ab4da26b11bae0547f96d6
joaobragaciandt <joaobraga@ciandt.com>
2022-06-14 10:04:05 -0300

    3. Status do Personagem

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,49 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if(getSaudeAtual()>0) {
+            this.status = "vivo";
+        }
+        else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        if(quantidadeDeDano>=getSaudeAtual()) {
+            setSaudeAtual(0);
+        }
+        else {
+            setSaudeAtual(getSaudeAtual()-quantidadeDeDano);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        if(quantidadeDeCura + getSaudeAtual()>= 100) {
+            setSaudeAtual(100);
+        }
+        else {
+            setSaudeAtual(getSaudeAtual()+quantidadeDeCura);
+        }
+    }
+}
