5114e4efcbfd2e484179ff9711f9dc8c495253dc
joaobragaciandt <joaobraga@ciandt.com>
2022-06-20 13:55:03 -0300

    new directory

--- 0x10/comida/Lembas.java
+++
@@ -1,8 +0,0 @@
-package comida;
-
-public class Lembas extends Comida {
-
-    public Lembas() {
-        super(3);
-    }
-}
--- 0x10/humor/Irritado.java
+++
@@ -1,4 +0,0 @@
-package humor;
-
-public class Irritado extends Humor{
-}
