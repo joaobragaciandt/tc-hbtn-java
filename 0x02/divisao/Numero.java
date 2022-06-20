fc63bfc0377bda4f3d0894d62b3e1c77f263e310
joaobragaciandt <joaobraga@ciandt.com>
2022-06-20 17:55:09 -0300

    2. Letra Maiúscula

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -2,12 +2,14 @@ import java.lang.ArithmeticException;
 
 public class Numero {
     public static void dividir(int a, int b) {
+        int c=0;
         try {
-            int c = a/b;
+            c = a/b;
         } catch (ArithmeticException e) {
             System.out.println("nao eh possivel dividir por 0");
+            c= 0;
         } finally {
-            System.out.print(a + " / " + b + " = " + a/b + "\n");
+            System.out.println(a + " / " + b + " = " + c);
         }
     }
 }
