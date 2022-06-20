e5f1ab8656e7a5e6061f8f201617b8544952e93d
joaobragaciandt <joaobraga@ciandt.com>
2022-06-20 15:22:58 -0300

    1. Numeros pares de 0 a 99

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,9 @@
+public class NumerosPares {
+    public static void main(String args[]) {
+        int i;
+        for(i=0;i<97;i+=2) {
+            System.out.print(i + ", ");
+        }
+        System.out.printf("%d\n",i);
+    }
+}
