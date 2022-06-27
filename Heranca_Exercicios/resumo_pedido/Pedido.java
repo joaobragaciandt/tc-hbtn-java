2b2c8adbf83715db59b84e26b87f3c465053cdf1
joaobragaciandt <joaobraga@ciandt.com>
2022-06-27 14:31:50 -0300

    1. Resumo Pedido

---
+++ Heranca_Exercicios/resumo_pedido/Pedido.java
@@ -0,0 +1,50 @@
+import produtos.*;
+
+public class Pedido {
+
+    private int percentualDescontos;
+    private ItemPedido[] itens;
+
+    public Pedido(int percentualDescontos, ItemPedido[] itens) {
+        this.percentualDescontos = percentualDescontos;
+        this.itens = itens;
+    }
+
+    public double totalPorProduto(ItemPedido i) {
+        return (i.getQuantidade() * i.getProduto().obterPrecoLiquido());
+    }
+
+    public double valorSemDesconto() {
+        double valorTotal = 0;
+        for(ItemPedido i : this.itens) {
+            valorTotal = valorTotal + totalPorProduto(i);
+        }
+        return valorTotal;
+    }
+
+    public double calcularTotal() {
+        double valorTotal = valorSemDesconto() - ( valorSemDesconto() * percentualDescontos/100);
+        return valorTotal;
+    }
+
+
+    public int getPercentualDescontos() {
+        return percentualDescontos;
+    }
+
+    public void apresentarResumoPedido(){
+        System.out.println("------- RESUMO PEDIDO -------");
+        for(ItemPedido i : this.itens) {
+            Produto produto = i.getProduto();
+            System.out.printf("Tipo: %s Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f\n",produto.getClass().getSimpleName(),
+                        produto.getTitulo(),produto.obterPrecoLiquido(),i.getQuantidade(),totalPorProduto(i));
+        }
+        System.out.println("----------------------------");
+        System.out.printf("DESCONTO: %d\n",this.getPercentualDescontos());
+        System.out.printf("TOTAL PRODUTOS: %.2f\n",this.valorSemDesconto());
+        System.out.println("----------------------------");
+        System.out.printf("TOTAL PEDIDO: %.2f\n",this.calcularTotal());
+        System.out.println("----------------------------");
+
+    }
+}
