6ccc6b751d4d325eec2455a60ccf7a7326e7af33
joaobragaciandt <joaobraga@ciandt.com>
2022-07-11 15:56:14 -0300

    1. Relatório Produto com maior preço

---
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(produto -> produto.getCategoria().equals(CategoriaProduto.LIVRO))
+                .collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream().max(Comparator.comparingDouble(Produto::getPreco)).orElse(null);
+    }
+}
