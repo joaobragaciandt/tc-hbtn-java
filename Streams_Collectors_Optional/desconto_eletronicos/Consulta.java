aa9fbd0710d9bf5d8c93f98cc74163e2683f7031
joaobragaciandt <joaobraga@ciandt.com>
2022-07-11 20:54:05 -0300

    4. Eletronicos com 15% de desconto

---
+++ Streams_Collectors_Optional/desconto_eletronicos/Consulta.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(produto -> produto.getCategoria().equals(CategoriaProduto.LIVRO))
+                .collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream().max(Comparator.comparingDouble(Produto::getPreco)).orElse(null);
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double preco) {
+        return produtos.stream().filter(produto -> produto.getPreco()>= preco).collect(Collectors.toList());
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
+        return pedidos.stream()
+                .filter(pedido -> pedido.getProdutos().stream()
+                        .anyMatch(produto -> produto.getCategoria().equals(CategoriaProduto.ELETRONICO)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Produto> aplicar15PorcentoDescontoEletronicos(List<Produto> produtos) {
+        return produtos.stream()
+                .peek(produto -> {
+                    if( produto.getCategoria().equals(CategoriaProduto.ELETRONICO)) {
+                        produto.setPreco(produto.getPreco() * 0.85);
+                    }
+                    produto.setPreco(produto.getPreco());
+                })
+                .collect(Collectors.toList());
+        //produto -> produto.setPreco(produto.getPreco()* 0.85)
+        //produto -> produto.getCategoria().equals(CategoriaProduto.ELETRONICO)
+    }
+}
