ac17fae653059d358ecd1828b4723dd723bc476e
joaobragaciandt <joaobraga@ciandt.com>
2022-07-11 15:45:55 -0300

    0. Relatórios Livros

---
+++ Streams_Collectors_Optional/livros/Consulta.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(produto -> produto.getCategoria().equals(CategoriaProduto.LIVRO))
+                .collect(Collectors.toList());
+    }
+
+}
