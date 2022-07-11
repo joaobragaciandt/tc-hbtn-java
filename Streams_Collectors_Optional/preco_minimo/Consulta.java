2b3af7073fab66e083b1ad326f5f9f122259d8fe
joaobragaciandt <joaobraga@ciandt.com>
2022-07-11 16:01:58 -0300

    2. Relatório Produtos por preco minimo

---
+++ Streams_Collectors_Optional/preco_minimo/Consulta.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(produto -> produto.getCategoria().equals(CategoriaProduto.LIVRO))
+                .collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream().max(Comparator.comparingDouble(Produto::getPreco)).orElse(null);
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double preco) {
+        return produtos.stream().filter(produto -> produto.getPreco()>= preco).collect(Collectors.toList());
+    }
+}
