62bf5d698334402a0b28687dc30fdd3a90babd1f
joaobragaciandt <joaobraga@ciandt.com>
2022-07-12 14:43:36 -0300

    1. Contagem de pessoas por cargo

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> todasPessoas) {
+
+        TreeMap<String, TreeSet<Pessoa>> retorno  = todasPessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo,() -> new TreeMap<>(Comparator.reverseOrder()),Collectors.toCollection(TreeSet::new)));
+        return retorno;
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> todasPessoas) {
+        return todasPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo,Collectors.counting()));
+    }
+}
+
