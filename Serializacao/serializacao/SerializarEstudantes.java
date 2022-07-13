aa6511f764904ec1695e416b211b2c7b21f9fab3
joaobragaciandt <joaobraga@ciandt.com>
2022-07-13 17:54:40 -0300

    0. Serialização de Estudantes

--- Serializacao/serializacao/SerializarEstudantes.java
+++ Serializacao/serializacao/SerializarEstudantes.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-
+@SuppressWarnings("unchecked")
 public class SerializarEstudantes<Estudante> {
 
     private final String nomeArquivo;
